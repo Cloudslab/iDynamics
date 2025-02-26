0a1d318c16fcea3ce45322ca5f56bdc61ca1dcd5
MingCHEN-Github <928163755@qq.com>
2025-02-26 23:59:21 +0000

    add abstract basic class for policy extention

---
+++ Evaluations/policyExtender/AbstractPolicyClass.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+class NodeInfo:
+    def __init__(self, node_name, cpu_capacity, mem_capacity, current_cpu_usage, current_mem_usage,
+                 network_latency, network_bandwidth):
+        self.node_name = node_name
+        self.cpu_capacity = cpu_capacity
+        self.mem_capacity = mem_capacity
+        self.current_cpu_usage = current_cpu_usage
+        self.current_mem_usage = current_mem_usage
+        self.network_latency = network_latency       # Could be a dict
+        self.network_bandwidth = network_bandwidth   # Could be a dict
+
+class PodInfo:
+    def __init__(self, pod_name, cpu_req, mem_req, sla_latency_requirement, deployment_name=None):
+        self.pod_name = pod_name
+        self.cpu_req = cpu_req
+        self.mem_req = mem_req
+        self.sla_latency_requirement = sla_latency_requirement
+        self.deployment_name = deployment_name  # helpful if need to patch the deployment
+
+class SchedulingDecision:
+    def __init__(self, pod_name, selected_node):
+        self.pod_name = pod_name
+        self.selected_node = selected_node
+
+class AbstractSchedulingPolicy(ABC):
+    @abstractmethod
+    def initialize_policy(self, config: dict) -> None:
+        """
+        Called once when the policy is created.
+        """
+        pass
+
+    @abstractmethod
+    def schedule_pod(self, pod: PodInfo, candidate_nodes: List[NodeInfo]) -> SchedulingDecision:
+        """
+        Return a SchedulingDecision for a single pod.
+        """
+        pass
+
+    @abstractmethod
+    def schedule_all(self, pods: List[PodInfo], candidate_nodes: List[NodeInfo]) -> List[SchedulingDecision]:
+        """
+        *Optional extension.* Return scheduling decisions for a batch of Pods simultaneously.
+        This is useful for multi-service or multi-pod optimization.
+        """
+        pass
+    
+    @abstractmethod
+    def on_update_metrics(self, nodes: List[NodeInfo]) -> None:
+        """
+        Called periodically if metrics are updated at runtime.
+        """
+        pass
